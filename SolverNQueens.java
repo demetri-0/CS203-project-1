304a2350341f4b5f072700697ef0b7104331ec5c
Demetri Karras <108017719+demetri894@users.noreply.github.com>
2025-05-11 14:38:26 -0400

    reworked random reshuffling

--- SolverNQueens.java
+++ SolverNQueens.java
@@ -15,19 +15,28 @@ public class SolverNQueens {
 
         System.out.print("Enter the dimension of the chess board (n > 3): ");
         int dimension = scan.nextInt();
+        while (dimension <= 3) {
+            System.out.print("Dimension too small. Try again (n > 3): ");
+            dimension = scan.nextInt();
+        }
 
+        long startTime = System.nanoTime();
         Chessboard chessboard = new Chessboard(dimension);
 
-        int swapCount = 0; // number of swaps performed
-        int boardShuffleCount = 0; // number of boards that have been created (not including the first)
+        int totalSwapCount = 0; // number of swaps performed leading to a solved board
+        int currentSwapCount = 0; // number of swaps performed on current iteration
+        int restartCount = 0; // number of times the algorithm runs on a "new" board
+        int boardShuffleCount = 0; // number of times a new board needed to be randomly generated
 
         /*
         The outermost loop runs while there are still colliding queens on the chessboard. All possible queen swaps are
         iterated through, checking if they reduce the number of attacks on the board. If attacks are reduced, the swap
-        is performed. If the board is not solved after checking and performing swaps, it is randomly reshuffled, and the
-        process repeats.
+        is performed. If after the first round of swaps the board is not solved, the entire process repeats until
+        the board is solved. If no swaps were performed on the outermost iteration, the board is randomly reshuffled.
          */
         while (chessboard.getTotalAttacks() > 0) {
+
+            currentSwapCount = 0;
 
             for (int queen1Row = 0; queen1Row < dimension; queen1Row++) {
 
@@ -38,20 +47,30 @@ public class SolverNQueens {
                         if (chessboard.swapReducesAttacks(queen1Row, queen2Row)) {
 
                             chessboard.swapQueenColumns(queen1Row, queen2Row);
-                            swapCount++;
+                            currentSwapCount++;
+                            totalSwapCount++;
                         }
                     }
                 }
             }
 
-            // if all collisions have not been eliminated after all performed swaps, the board is reshuffled
+            // if the board is unsolved, it may be reshuffled
             if (chessboard.getTotalAttacks() > 0) {
-                chessboard = new Chessboard(dimension);
-                boardShuffleCount++;
+
+                if (currentSwapCount == 0) { // algorithm is "stuck", new board is randomly generated
+                    chessboard = new Chessboard(dimension);
+                    boardShuffleCount++;
+                }
+
+                restartCount++;
             }
         }
 
-        System.out.println(swapCount + " total swaps were performed. The board needed to be shuffled " + boardShuffleCount + " times.");
-    }
+        long endTime = System.nanoTime();
 
+        System.out.println(totalSwapCount + " total swaps were performed.");
+        System.out.println("The algorithm ran " + restartCount + " additional time(s).");
+        System.out.println("The board was reshuffled " + boardShuffleCount + " time(s).");
+        System.out.println("Execution Time: " + (endTime - startTime) / 1000000 + " ms");
+    }
 }
